21f0d63996f1636c19177ab2b2cdba3dced05b61
aggarwalarshia <160538181+aggarwalarshia@users.noreply.github.com>
2024-07-02 13:42:16 +0000

    learnt types of dataTypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treats all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+// alert is used to pop-up a text on website
+
+// console.log("Arshia") console.log(3+3) //this will not work
+// console.log("Arshia"); console.log(3+3) //not a good practice
+
+//ORIGINAL DOCUMENTATION OF JAVASCRIPT IS 'ECMAScript' (Standards)
+//you can also refer to MDN Docs
+
+// TYPES OF DATATYPES
+
+let name = "arshia"    // 1. string
+let age = 21           // 2. integer
+let isLoggedIn = false // 3. boolean
+let state = null       // 4. null is a standalone value
+let marks;             // 5. undefined
+// 6. bigint
+// 7. symbol- unique (will learn in react)
+// 8. object (will discuss in future lectures)
+
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); // object
+
+// NOTE: typeof is used to obtain the type of dataType of the variable mentioned
