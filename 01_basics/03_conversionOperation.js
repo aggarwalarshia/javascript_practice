98e1ab7d632ee2ec765a5787cb54ac02863b3b78
aggarwalarshia <160538181+aggarwalarshia@users.noreply.github.com>
2024-07-03 20:04:27 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+// let score = 33
+// let score1 = "33"
+// console.log(typeof score)
+// console.log(typeofscore1)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) //converted to number
+
+// let score2 = "33abc"
+// let changeToNumber = Number(score2);
+
+// console.log(typeof changeToNumber) // Number
+// console.log(changeToNumber)  //NaN
+
+// in js, the string type values cannot be converted to number type sometimes
+
+// let score = null
+// console.log(typeof score) // object
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number
+// console.log(valueInNumber) // 0 (zero)
+
+// let score = undefined
+// console.log(typeof score) // undefined
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number
+// console.log(valueInNumber) // NaN (Not a Number)
+
+// let score = true
+// console.log(typeof score) // boolean
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number
+// console.log(valueInNumber) // 1 (true converted to 1)
+
+// let score = "arshia"
+// console.log(typeof score) // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // number
+// console.log(valueInNumber) // NaN (Not a Number)
+
+// NOTES:
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn); 
+// console.log(booleanIsLoggedIn); // true
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn); 
+// console.log(booleanIsLoggedIn); // false
+
+// let isLoggedIn = "arshia"
+// let booleanIsLoggedIn = Boolean(isLoggedIn); 
+// console.log(booleanIsLoggedIn); // true
+
+// NOTES:
+
+// 1 => true; 0 => false
+// "" => false
+// "arshia" => true
+
+// let someNumber = 33
+// let stringNumber =String(someNumber);
+// console.log(typeof stringNumber); // string
+// console.log(stringNumber); // 33
