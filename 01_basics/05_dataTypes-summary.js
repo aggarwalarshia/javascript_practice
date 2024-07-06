6894e08e59556dde920bf7a003df346507699bcd
aggarwalarshia <160538181+aggarwalarshia@users.noreply.github.com>
2024-07-06 20:40:56 +0000

    dataTypes complete

---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,53 @@
+// PRIMITIVE DATATYPES
+// 7 types : 
+// String, Number, Boolean, null, undefined, 
+// Symbol(kisi bhi value ko unique banane k liye symbol use hota hai), BigInt
+
+// const score = 100;
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let userEamil;
+
+// DECLARE SYMBOL:
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+// // now check if these two symbols are same
+// console.log(id === anotherId); //false
+
+// bigInt
+// const bigNumber = 34567891012115675434n;
+// NOTE: adding n at the end automatically stores value as bigInt
+
+// Reference (Non-PRIMITIVE)
+// Array, Objects, Functions
+
+// Array ->
+// const heros = ["shaktiman", "naagraj", "doga"];
+
+// Object ->
+// let myObj = {
+//     name: "arshia",
+//     age: 21,
+// }
+
+// Function ->
+// const myFunction = function()
+// {
+//     console.log("hello javascript");
+// }
+
+// JavaScript is dynamically typed language or statically typed?
+// -> dynamically typed (bcz we need not to define dataTypes of variables)
+
+// To check dataType of a variable:
+// typeof 
+// console.log(typeof number); // integer
+// console.log(typeof null); // object
+// console.log(typeof myFunction); // function
+
+
+
+
+// *********READ ECMAScript!!!!!!!!!!!!!!!!!*******
+// *********READ MDN Docs**********
