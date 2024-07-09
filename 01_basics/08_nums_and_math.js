ce22017c7993a5f83e10a1478e45573eeb21d121
aggarwalarshia <160538181+aggarwalarshia@users.noreply.github.com>
2024-07-09 08:10:54 +0000

    pending

---
+++ 01_basics/08_nums_and_math.js
@@ -0,0 +1,37 @@
+// NUMBERS AND MATHS
+
+// const score = 400;
+// console.log(score); // 400
+// const balance = new Number(100);
+// console.log(balance); // [Number: 100]
+
+// console.log(balance.toString().length); // 3
+
+// console.log(balance.toFixed(2)); // 100.00
+
+// const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(3)); // 124 (to 3 places that is 123, not 123.8 or 123.89 etc etc)
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); // 1,000,000
+// console.log(hundreds.toLocaleString('en-IN')); // 10,00,000 (india standard)
+
+// CHECK OTHER FUNCTIONS OR FEATURES LIKE THESE ON MDN DOCS
+
+// MAX AND MIN (run it on browser console)
+// Number.MIN_VALUE; // 5e-324
+// Number.MAX_VALUE; // 1.7976931348623157e+308
+// Number.MAX_SAFE_INTEGER // 9007199254740991
+
+
+// ******MATHS******(TRY IT ON BROWSER CONSOLE)
+
+console.log(Math); // there u will get to know a variety of things about Math
+// like
+console.log(Math.abs(-33)); // 33 (absolute value)
+console.log(Math.round(4.4567)); // 4
+console.log(Math.round(4.5)); // 5
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(4, 3, 6, 8)); // 3
+
